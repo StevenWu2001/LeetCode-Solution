b39ce6f3ae8cc87020ddb0340f434e42cfe8968f
Steven Wu <70414253+StevenWu2001@users.noreply.github.com>
2022-09-01 17:14:09 -0400

    Create 73-Set-Matrix-Zeros.java

---
+++ Medium/73-Set-Matrix-Zeros.java
@@ -0,0 +1,61 @@
+// Time: O(mn)
+// Space: O(1)
+// Use first row and col as indicator, instead of allocating additional memory
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length, col = matrix[0].length;
+        boolean firstCol = false, firstRow = false;
+        
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                firstCol = true;
+            }
+        }
+        
+        for (int j = 0; j < col; j++) {
+            if (matrix[0][j] == 0) {
+                firstRow = true;
+            }
+        }
+        
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for (int i = 1; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < col; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        for (int j = 1; j < col; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < row; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    
+        
+        if (firstRow) {
+            for (int j = 0; j < col; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if (firstCol) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
