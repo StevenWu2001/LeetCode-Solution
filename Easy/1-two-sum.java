4b35d964d139dd76bfadb84e14cd08697e78018e
Steven Wu <70414253+StevenWu2001@users.noreply.github.com>
2022-09-05 18:04:18 -0400

    Create 1-two-sum.java

---
+++ Easy/1-two-sum.java
@@ -0,0 +1,20 @@
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                ans[0] = i;
+                ans[1] = map.get(target - nums[i]);
+                break;
+            }
+            map.put(nums[i], i);
+        }
+        
+        return ans;
+    }
+}
