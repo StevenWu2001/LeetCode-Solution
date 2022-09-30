552c6129cdc3c5e3f7cb2db23fddd4a63e7c1846
Steven Wu <70414253+StevenWu2001@users.noreply.github.com>
2022-09-29 20:05:23 -0400

    Create 11-Container-With-Most-Water.java

---
+++ Medium/11-Container-With-Most-Water.java
@@ -0,0 +1,20 @@
+// Two Pointer
+// O(N)
+
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = Math.min(height[l], height[r]) * (r - l);
+        
+        while (l < r) {
+            if (height[l] <= height[r]) {
+               l++; 
+            } else {
+                r--;
+            }
+            ans = Math.max(ans, Math.min(height[l], height[r]) * (r - l));
+        }
+        
+        return ans;
+    }
+}
