86ed51bb0eb3839c6d610754540b87c1abc7b32f
Shengyang (Steven) Wu <70414253+StevenWu2001@users.noreply.github.com>
2022-10-17 10:06:44 -0400

    Create 20-Number-of-Islands.java

---
+++ Medium/20-Number-of-Islands.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    ans++;
+                    helper(grid, i, j);
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+    public void helper(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        helper(grid, i + 1, j);
+        helper(grid, i - 1, j);
+        helper(grid, i, j + 1);
+        helper(grid, i, j - 1);
+    }
+}
