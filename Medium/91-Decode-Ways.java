99d76572de1257a18dc41837ae035f86f92fd887
Shengyang (Steven) Wu <70414253+StevenWu2001@users.noreply.github.com>
2022-10-27 00:02:52 -0400

    Update 91-Decode-Ways.java

--- Medium/91-Decode-Ways.java
+++ Medium/91-Decode-Ways.java
@@ -1,3 +1,4 @@
+// Solution 1
 class Solution {
     public int numDecodings(String s) {
         int a = 0, b = 1;
@@ -19,5 +20,36 @@ class Solution {
         }
         
         return a;
+    }
+}
+
+// Solution 2, more intuitive, but uses O(n) space.
+class Solution {
+    public int numDecodings(String s) {
+        if (s.length() == 1) {
+            return s.charAt(0) == '0' ? 0 : 1;
+        }
+        
+        int[] dp = new int[s.length()];
+        dp[s.length() - 1] = s.charAt(s.length() - 1) == '0' ? 0 : 1;
+        dp[s.length() - 2] = dp[s.length() - 1];
+        
+        if (s.charAt(s.length() - 2) == '1' || (s.charAt(s.length() - 2) == '2' && s.charAt(s.length() - 1) < '7')) {
+            dp[s.length() - 2] = 1 + dp[s.length() - 1];
+        } else if (s.charAt(s.length() - 2) == '0') {
+            dp[s.length() - 2] = 0;
+        }
+        
+        for (int i = s.length() - 3; i >= 0; i--) {
+            if (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
+                dp[i] = dp[i + 1] + dp[i + 2];
+            } else if (s.charAt(i) == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+            }
+        }
+        
+        return dp[0];
     }
 }
