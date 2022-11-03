5cd7e82497c31a62ffbcfa229f643f47b5dd633a
Shengyang (Steven) Wu <70414253+StevenWu2001@users.noreply.github.com>
2022-11-02 23:02:22 -0400

    Create 338-Counting-Bits.java

---
+++ Easy/338-Counting-Bits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            ans[i] = bit(i);
+        }
+        return ans;
+    }
+    
+    public int bit(int n) {
+        int ans = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            ans++;
+        }
+        return ans;
+    }
+}
